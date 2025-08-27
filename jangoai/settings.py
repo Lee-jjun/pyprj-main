a62ec58483e4864da3a59ed2e40b882350fcc021
leejj <abc1058711@gmail.com>
2025-08-27 14:29:49 +0900

    settings.py, urls.py 수정사항 없음

--- jangoai/settings.py
+++ jangoai/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'jangoai.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'bbs'/'templates'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
